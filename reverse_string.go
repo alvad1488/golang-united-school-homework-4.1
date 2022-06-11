59855d292131c81c0356904175d522e132db1d8e
CODEINUS59 <alexvad1488@gmail.com>
2022-06-11 19:33:53 +0300

    task done

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,17 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+
+	check := []rune(input)
+	length := len(check)
+
+	if length > 0 {
+		for i := 0; i < length/2; i++ {
+			tmp := check[length-1-i]
+			check[length-1-i] = check[i]
+			check[i] = tmp
+		}
+	}
+	output = string(check)
+	return
 }
